61ebb6e53f8d486a10323d5dba3c73206129411c
Akhetopnu <akhetopnu@protonmail.com>
2018-10-05 19:36:07 +0200

    (+ storage)

---
+++ src/storage.js
@@ -0,0 +1,22 @@
+export function user () {
+  const str = sessionStorage.getItem('user`');
+  if (!str) {
+    console.warn('You are a guest.');
+    return;
+  }
+
+  try {
+    return JSON.parse(str);
+  } catch (error) {
+    console.error('Failed to parse sessionStorage value.');
+    return;
+  }
+}
+
+export function set (user) {
+  if (Object.prototype.toString.call(user) !== '[object Object]') {
+    throw new Error('What do you think you\'re doing?')
+  }
+
+  sessionStorage.setItem('user', JSON.stringify(user));
+}
