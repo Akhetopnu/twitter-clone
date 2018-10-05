92907a8a6aaae1ddcc0ab032d17fa353399cac6d
Akhetopnu <akhetopnu@protonmail.com>
2018-10-05 19:37:14 +0200

    (components (+ login))

---
+++ src/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <span>Login</span>
+          <input type='text' pattern='.{5,}' required />
+        </div>
+        <div>
+          <span>Password:</span>
+          <input type='password' pattern='' title='Password must be at least 8 characters long, contains at least 1 small letter, one capital letter and 1 number' required />
+        </div>
+      </div>
+    );
+  }
+}
