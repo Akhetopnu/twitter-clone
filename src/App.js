4d0c64608756341da3450b97443b07ab6e5d357a
Akhetopnu <akhetopnu@protonmail.com>
2018-10-05 19:38:19 +0200

    (components (+ skeleton (app)))

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
+import Home from './Home';
+import Wall from './Wall';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <div>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/login'>Login</Link></li>
+              <li><Link to='/wall'>Wall</Link></li>
+            </ul>
+          </div>
+          <hr />
+
+          <Route exact path='/' component={Home} />
+          <Route path='/login' component={Login} />
+          <Route path='/Wall' component={Wall} />
+        </div>
+      </Router>
     );
   }
 }
-
-export default App;
