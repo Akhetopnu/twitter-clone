8fa793fab18a82084b9b8fff6f2ad28685cead30
Akhetopnu <akhetopnu@protonmail.com>
2018-10-06 18:36:07 +0200

    (components (tweet (+ popup-error (if (not-found post)))))

--- src/components/wall/tweet/Tweet.jsx
+++ src/components/wall/tweet/Tweet.jsx
@@ -1,6 +1,7 @@
 import './Tweet.scss'
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { PopupError } from '../../popup-error/PopupError.jsx';
 import * as API from '../../../services/api';
 
 export class Tweet extends Component {
@@ -10,19 +11,27 @@ export class Tweet extends Component {
     this.state = {
       id,
       post: undefined,
+      exists: false,
     }
   }
-  componentWillMount() {
-    API
-      .fetch_by_id(this.state.id)
-      .then(post => {
-        setTimeout(() => {
-          this.setState({ post });
-        }, 300);
+  async fetch() {
+    const post = await API.fetch_by_id(this.state.id)
+      .catch(console.error);
+    if (!post) {
+      this.setState({
+        exists: false,
       });
+    }
+
+    this.setState({ post });
+  }
+
+  componentWillMount() {
+    this.fetch();
   }
 
   render() {
+    const { post } = this.state;
     return (
       <div className='post'>
         <header className='post-header'>
@@ -31,16 +40,21 @@ export class Tweet extends Component {
           </Link>
         </header>
 
-      {this.state.post
-        ?
-          <div>
-            <p><b>UserId:</b> userId{this.state.post.userId}</p>
-            <p><b>Id:</b> {this.state.post.id}</p>
-            <p><b>Title:</b> {this.state.post.title}</p>
-            <p><b>Body:</b> {this.state.post.body}</p>
-          </div>
-        :
-          <h2>Loading...</h2>
+      {!this.exists
+        ? <PopupError
+            title='404 Not found'
+            message='Sorry, we could not find post with an id of 0.'
+          />
+        : this.state.post
+          ?
+            <div>
+              <p><b>UserId:</b> userId{post.userId}</p>
+              <p><b>Id:</b> {post.id}</p>
+              <p><b>Title:</b> {post.title}</p>
+              <p><b>Body:</b> {post.body}</p>
+            </div>
+          :
+            <h2>Loading...</h2>
       }
       </div>
     );
