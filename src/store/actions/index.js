e0b358773c60eb906a7b212f7da6bfd86e913d24
Akhetopnu <akhetopnu@protonmail.com>
2018-10-07 13:25:12 +0200

    (store (actions (+ export for creators) (~ using creators instead of inline object literals)))

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,23 +1,19 @@
 import * as API from '../../services/api';
 import * as types from './types';
+import * as creators from './creators';
 
 export * from './types';
+export { creators };
 
 export const getCount = () => async dispatch => {
   const posts_count = await API.fetch_count();
 
-  dispatch({
-    type: types.POSTS_INIT,
-    payload: posts_count,
-  });
+  dispatch(creators.POSTS_INIT(posts_count));
 };
 
 export const get = id => async dispatch => {
   const post = await API.fetch_by_id(id);
 
-  dispatch({
-    type: types.POST_ADD,
-    payload: post,
-  });
+  dispatch(creators.POST_ADD(post));
 };
 
