b643929e2a839c8d963e7fc9b8e9fc487f4fa58c
Akhetopnu <akhetopnu@protonmail.com>
2018-10-07 10:29:15 +0200

    (components (tweet-preview (refactored/formatting)))

--- src/components/wall/tweet-preview/TweetPreview.jsx
+++ src/components/wall/tweet-preview/TweetPreview.jsx
@@ -1,19 +1,20 @@
 import './TweetPreview.scss';
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export class TweetPreview extends Component {
   render() {
     const { userId, title, id, hidden } = this.props.item;
     const klass = 'tweet-preview' + (hidden ? ' --hidden' : '');
+    if (!userId || !title || !id) {
+      return <div className='tweet-preview hidden'></div>;
+    }
 
-    return userId && title && id
-      ?
-        <Link className={klass} to={'/wall/' + id}>
-          <h2>{userId ? 'UserId: ' + userId : ''}</h2>
-          <span>{title || ''}</span>
-        </Link>
-      :
-        <div className='tweet-preview hidden'></div>
+    return (
+      <Link className={klass} to={'/wall/' + id}>
+        <h2>{userId ? 'UserId: ' + userId : ''}</h2>
+        <span>{title || ''}</span>
+      </Link>
+    );
   }
 }
