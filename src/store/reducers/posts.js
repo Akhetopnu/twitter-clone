9bdcaeec9f9f9f4486a941302247da830996a568
Akhetopnu <akhetopnu@protonmail.com>
2018-10-07 11:47:48 +0200

    (store (posts (reducers (removed console.logs))))

--- src/store/reducers/posts.js
+++ src/store/reducers/posts.js
@@ -8,14 +8,12 @@ const stateInitial = {
 export function reducer(state = stateInitial, { type, payload }) {
   switch (type) {
     case POSTS_INIT: {
-      console.log('posts init!', payload);
       return {
         ...state,
         next: payload,
       };
     }
     case POST_ADD: {
-      console.log('adding a new post:', payload, [...state.list, payload]);
       return {
         ...state,
         list: [...state.list, payload],
