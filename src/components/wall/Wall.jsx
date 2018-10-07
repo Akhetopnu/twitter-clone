664b5b610cb58855c244626996c16396c1cacbcf
Akhetopnu <akhetopnu@protonmail.com>
2018-10-07 11:52:59 +0200

    (components (wall (connected to redux store)))

--- src/components/wall/Wall.jsx
+++ src/components/wall/Wall.jsx
@@ -1,84 +1,77 @@
 import './Wall.scss';
-import * as API from '../../services/api';
 import * as auth from '../../services/auth';
+import * as actions from '../../store/actions';
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { TweetPreview } from './tweet-preview/TweetPreview.jsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Tweet } from './tweet/Tweet.jsx';
+import { connect } from 'react-redux';
+import { store } from '../../store';
 
-export class Wall extends Component {
-  constructor({ match }) {
-    super();
-
-    this.state = {
-      list: [],
-      search: '',
-      post_list: [],
-      post_id: match.params.id,
-      post_current: 0,
-      post_count: 0,
-    };
-    this.is_streaming = false;
-    this.is_mounted = true;
-  }
-
+class $Wall extends Component {
   get list() {
-    return [
-      ...this.state.post_list,
-      {},
-    ].map(item => ({
+    return [...this.props.posts.list, {}].map(item => ({
       ...item,
       hidden: !this.isMatch(item),
     }));
   }
 
-  async stream() {
-    const post_count = await API.fetch_count();
-    this.setState({
-      post_count,
-      post_current: post_count,
-    });
+  constructor({ match }) {
+    super();
 
-    this.stream_tick();
+    this.state = {
+      search: '',
+      post_id: match.params.id,
+    };
+
+    this.is_streaming = false;
+    this.is_mounted = true;
+    this.timer = NaN;
+    this.store_unsubscribe = store.subscribe(() => {
+      const { next } = store.getState().posts;
+      if (this.is_mounted && this.is_streaming) {
+        if (next) {
+          this.timer = setTimeout(() => {
+            this.props.postGet(next);
+          }, 2e3);
+        } else {
+          console.info('No more posts available.');
+        }
+      }
+    });
   }
 
-  stream_finish() {
-    console.info('No more posts available.');
-  }
-
-  async stream_tick() {
-    if (this.state.post_current === 0) {
-      return this.stream_finish();
-    }
-
-    const post = await API.fetch_by_id(this.state.post_current);
-    if (!Object.keys(post).length) {
-      return this.stream_finish();
+  componentWillReceiveProps({ match }) {
+    this.is_mounted = this.is_streaming = !match.params.id;
+    if (!this.is_streaming) {
+      clearTimeout(this.timer);
     }
 
     this.setState({
-      post_list: [...this.state.post_list, post],
-      post_current: this.state.post_current - 1,
+      post_id: match.params.id,
     });
-
-    if (!this.is_mounted || !this.is_streaming) {
-      return;
-    }
-
-    setTimeout(() => {
-      this.stream_tick();
-    }, 2e3)
   }
 
   componentDidMount() {
-    this.is_mounted = true;
-    this.stream();
+    if (this.state.post_id) {
+      return;
+    }
+
+    if (store.getState().posts.next === undefined) {
+      return void this.props.postGetCount();
+    }
+    if (this.props.posts.next <= 0) {
+      return console.log('wall mounted but no more posts to fetch.');
+    }
+
+    this.props.postGet(this.props.posts.next);
   }
 
   componentWillUnmount() {
     this.is_mounted = false;
+    this.store_unsubscribe();
   }
 
   render() {
@@ -92,6 +85,7 @@ export class Wall extends Component {
     }
 
     this.is_streaming = true;
+
     return (
       <div className='wall'>
 
@@ -114,15 +108,8 @@ export class Wall extends Component {
     );
   }
 
-  componentWillReceiveProps({ match: { params: { id } } }) {
-    if (id) {
-      this.setState({
-        post_id: id,
-      });
-    }
-  }
-
-  isMatch({ title, body }) {
+  isMatch(post) {
+    const { title, body } = post;
     const { search } = this.state;
 
     return !search || (!title && !body) || (
@@ -138,3 +125,10 @@ export class Wall extends Component {
     });
   }
 }
+
+export const Wall = connect(state => ({
+  posts: state.posts,
+}), {
+  postGet: actions.get,
+  postGetCount: actions.getCount,
+})($Wall);
